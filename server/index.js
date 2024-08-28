91752e2daeb4875999a9d5b7a6a07438a01bd84a
yash-070702 <yashaggarwal2002.ya@gmail.com>
2024-08-29 01:35:47 +0530

    Update CORS configuration

--- server/index.js
+++ server/index.js
@@ -30,7 +30,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
 	cors({
-		origin: "http://localhost:3000",
+		origin: "https://studynotion-woad.vercel.app",
 		credentials: true,
 	})
 );
