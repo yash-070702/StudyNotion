9da870890d2ca8f303d9e484157d71880d1e5a3d
yash-070702 <yashaggarwal2002.ya@gmail.com>
2024-08-28 18:05:34 +0530

    First Commit StudyNotion

--- server/controllers/Auth.js
+++ server/controllers/Auth.js
@@ -2,7 +2,7 @@ const User=require("../models/User");
 const Profile=require("../models/Profile")
 const OTP=require("../models/OTP");
 const otpGenerator=require("otp-generator");
-const bcrypt=require("bcrypt");
+const bcrypt = require('bcryptjs');
 const jwt=require("jsonwebtoken");
 require("dotenv").config();
 
